b1e848a7d3d824b95810f7ec65dbc1add6f14e13
Filip2525 <filip.stefko2525@gmail.com>
2025-05-06 21:32:03 +0200

    Update mouse.gd

--- Scripts/mouse.gd
+++ Scripts/mouse.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @export var speed = 10
+var direction
+
 
 func _physics_process(delta: float) -> void:
-	pass
+	direction = Vector2.from_angle(get_angle_to(get_node("/root/Cat").position)).normalized()
+	position += direction * speed * delta
